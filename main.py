93f2f93bffaea036695ea46d99f30dcfa1bb8347
NicSteph <nstephenson5@hawkmail.hccfl.edu>
2025-09-21 14:43:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ salary = float(input())
 numDependents = float(input())
 
 # calculate taxes here
-
+stateTax = salary * .065
+federalTax = salary * .28
+dependentDeduction = (salary * .025) * 2
+totalWithholding = stateTax + federalTax + dependentDeduction
+takeHomePay = salary - totalWithholding
 # output statements
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
